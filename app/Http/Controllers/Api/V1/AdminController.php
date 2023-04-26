0fca0cad2660d5555e2fb2658cea7fafe71d3277
pikapikamart <itsmelight1@gmail.com>
2023-04-26 16:17:43 +0800

    add api for creating tokens using env app pasword

--- app/Http/Controllers/Api/V1/AdminController.php
+++ app/Http/Controllers/Api/V1/AdminController.php
@@ -3,14 +3,46 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller {
 
   function createAdmin(Request $request) {
-    $appPassword = $request->input("appPassword");
-
+    $appPassword = $request->appPassword;
     
+    if (!$appPassword || env("APP_PASSWORD")!=$appPassword) {
+      return response()->json(["message" => "App password invalid"]);
+    }
+
+    $credentials = [
+      "email" => $request->email,
+      "password" => Hash::make($request->password),
+      "name" => $request->name
+    ];
+
+    $user = new User();
+    $user->name = $credentials["name"];
+    $user->email = $credentials["email"];
+    $user->password = Hash::make($credentials["password"]);
+
+    $user->save();
+
+    if (Auth::attempt($credentials)) {
+      $user = Auth::user();
+
+      if (!($user instanceof User)) return response()->json(["message" =>"Server error"]);
+
+      $adminToken = $user->createToken("admin-token", ["all"]);
+      $customerToken = $user->createToken("customer-token", ["customer"]);
+
+      return [
+        "admin" => $adminToken->plainTextToken,
+        "customer" => $customerToken->plainTextToken
+      ];
+    }
   }
 
 	public function index(){
