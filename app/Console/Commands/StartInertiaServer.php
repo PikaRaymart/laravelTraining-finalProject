1bd87681a109caf39788fdca64daf1f84793b871
pikapikamart <itsmelight1@gmail.com>
2023-05-22 11:32:11 +0800

    add command for starting all dev mode

---
+++ app/Console/Commands/StartInertiaServer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+
+class StartInertiaServer extends Command{
+
+	protected $signature = 'inertia:serve';
+	protected $description = 'Command description';
+
+  function handle(){
+    $this->startConcurrentServers();
+  }
+
+  protected function startConcurrentServers(){
+    $this->info('Starting Inertia.js SSR server, Laravel server, and npm dev concurrently...');
+
+    $inertiaSsrProcess = new Process(['php', 'artisan', 'inertia:start-ssr']);
+    $inertiaSsrProcess->setTimeout(null); // Set no timeout for the process
+    $inertiaSsrProcess->start();
+
+    $laravelServeProcess = new Process(['php', 'artisan', 'serve']);
+    $laravelServeProcess->setTimeout(null); // Set no timeout for the process
+    $laravelServeProcess->start();
+
+    $npmDevProcess = new Process(['npm', 'run', 'dev']);
+    $npmDevProcess->setTimeout(null); // Set no timeout for the process
+    $npmDevProcess->run();
+
+    $inertiaSsrProcess->wait();
+  }
+}
